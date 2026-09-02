4e22dabaca27ffac33f802e57576dfa21399b623
DarthNecrohorn <pedjasebez3545@gmail.com>
2026-09-02 16:29:27 +0200

    Update calculators.js

--- calculators.js
+++ calculators.js
@@ -2666,3 +2666,49 @@ function resetGeneric(){
         "Reset complete"
     );
 }
+
+function openCalculator(type){
+
+$("homePage").style.display = "none";
+
+$("weatherSection").style.display = "none";
+$("newsSection").style.display = "none";
+
+const settingsPage = $("settingsPage");
+
+if(settingsPage){
+    settingsPage.style.display = "none";
+}
+
+document.querySelectorAll(".app")
+    .forEach(x => x.classList.remove("active"));
+
+$("genericApp").style.display = "none";
+$("calculatorApp").style.display = "none";
+
+if(type === "basic" || type === "advanced" || type === "scientific"){
+
+    $("calculatorApp").style.display = "block";
+    $("calculatorApp").classList.add("active");
+
+    setCalculatorMode(type);
+
+}else{
+
+    $("genericApp").style.display = "block";
+    $("genericApp").classList.add("active");
+
+    setupGeneric(type);
+}
+
+window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+});
+
+document.documentElement.style.overflowY = "auto";
+document.body.style.overflowY = "auto";
+
+$("navLinks").classList.remove("open");
+
+}
