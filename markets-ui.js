16d7f4dfb396a6088cd778c1d097123af71c7c31
DarthNecrohorn <pedjasebez3545@gmail.com>
2026-09-12 15:01:05 +0200

    Update markets-ui.js

--- markets-ui.js
+++ markets-ui.js
@@ -745,7 +745,6 @@ async function refreshMarkets() {
                 "/api/markets",
                 {
                     method: "GET",
-                    cache: "no-store"
                 }
             ),
 
