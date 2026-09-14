207592a25425b87882d6960d2216cd6146d5c97f
DarthNecrohorn <pedjasebez3545@gmail.com>
2026-09-14 12:35:42 +0200

    Update shop-affiliates.js

--- shop-affiliates.js
+++ shop-affiliates.js
@@ -4,40 +4,65 @@
 
 const affiliateProducts = {
 
-    /* =================================================
-       BEAUTY
-    ================================================= */
+/* =================================================
+   BEAUTY
+===================================================== */
 
-    Beauty: [
+Beauty: [
 
-        {
-            id:
-                "skin1004-hyalu-cica-sun-serum",
+    {
+        id:
+            "skin1004-hyalu-cica-sun-serum",
 
-            title:
-                "SKIN1004 Madagascar Centella Hyalu-Cica Water-Fit Sun Serum SPF50+ PA++++ Twin Pack",
+        title:
+            "SKIN1004 Madagascar Centella Hyalu-Cica Water-Fit Sun Serum SPF50+ PA++++ Twin Pack",
 
-            oldPrice:
-                27.00,
+        oldPrice:
+            27.00,
 
-            price:
-                18.04,
+        price:
+            18.04,
 
-            currency:
-                "$",
+        currency:
+            "$",
 
-            store:
-                "Stylevana",
+        store:
+            "Stylevana",
 
-            image:
-                "",
+        image:
+            "",
 
-            affiliateUrl:
-                   "https://tidd.ly/46mlVcw"
-        }
+        affiliateUrl:
+            "https://tidd.ly/46mlVcw"
+    },
 
-    ],
+    {
+        id:
+            "beauty-of-joseon-relief-sun-set",
 
+        title:
+            "BEAUTY OF JOSEON - Relief Sun : Rice + Probiotics / Niacinamide Set SPF50+ PA++++ - 50ml*2",
+
+        oldPrice:
+            36.00,
+
+        price:
+            23.40,
+
+        currency:
+            "$",
+
+        store:
+            "Stylevana",
+
+        image:
+            "",
+
+        affiliateUrl:
+            "https://tidd.ly/4rcWH9W"
+    }
+
+],
 
     /* =================================================
        FASHION
