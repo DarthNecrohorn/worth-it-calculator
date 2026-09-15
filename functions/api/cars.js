419163edaeaf69eda546f9e921c702942a04eb21
DarthNecrohorn <pedjasebez3545@gmail.com>
2026-09-15 14:10:39 +0200

    Update cars.js

--- functions/api/cars.js
+++ functions/api/cars.js
@@ -108,6 +108,11 @@ export async function onRequestGet(context) {
         );
     }
 
+    imagesUrl.searchParams.set(
+    "license",
+    "ShareCommercially"
+);
+            
     const response = await fetch(
         imagesUrl.toString()
     );
