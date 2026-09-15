aa5f257906324e89f1e0f20f14a3d0283038c51d
DarthNecrohorn <pedjasebez3545@gmail.com>
2026-09-15 12:06:11 +0200

    Update cars-ui.js

--- cars-ui.js
+++ cars-ui.js
@@ -1,1 +1,70 @@
+function openCars() {
 
+    const homePage =
+        document.getElementById("homePage");
+
+    if (homePage) {
+        homePage.style.display = "none";
+    }
+
+    document.querySelectorAll(".app").forEach(x => {
+        x.classList.remove("active");
+        x.style.display = "none";
+    });
+
+    const weatherSection =
+        document.getElementById("weatherSection");
+
+    if (weatherSection) {
+        weatherSection.style.display = "none";
+    }
+
+    const newsSection =
+        document.getElementById("newsSection");
+
+    if (newsSection) {
+        newsSection.style.display = "none";
+    }
+
+    const discountsSection =
+        document.getElementById("discountsSection");
+
+    if (discountsSection) {
+        discountsSection.style.display = "none";
+    }
+
+    const settingsPanel =
+        document.getElementById("settingsPanel");
+
+    if (settingsPanel) {
+        settingsPanel.style.display = "none";
+    }
+
+    const marketsSection =
+        document.getElementById("marketsSection");
+
+    if (marketsSection) {
+        marketsSection.style.display = "none";
+    }
+
+    const moneySection =
+        document.getElementById("moneySection");
+
+    if (moneySection) {
+        moneySection.style.display = "none";
+    }
+
+    const carsSection =
+        document.getElementById("carsSection");
+
+    if (carsSection) {
+        carsSection.style.display = "block";
+    }
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+window.openCars = openCars;
