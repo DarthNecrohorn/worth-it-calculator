d7b0ab92509cde55e737480dc33f353edc06f5f8
DarthNecrohorn <pedjasebez3545@gmail.com>
2026-09-18 10:46:16 +0200

    Update cars.js

--- functions/api/cars.js
+++ functions/api/cars.js
@@ -25,7 +25,7 @@ const VEHICLES_DB_URL =
 const CACHE_TTL = 86400; // 24 hours
 const WIKIPEDIA_CACHE_TTL = 604800; // 7 days
 const MAX_MODELS_PER_KIND = 300; // Keep the catalog focused on popular vehicles
-const WIKIPEDIA_CACHE_VERSION = "v9";
+const WIKIPEDIA_CACHE_VERSION = "v10";
 
 const WIKIPEDIA_API =
     "https://en.wikipedia.org/w/api.php";
