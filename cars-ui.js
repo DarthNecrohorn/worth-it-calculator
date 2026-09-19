8dad6f80ccba0f7df83560cc175dd0cc7957e56b
DarthNecrohorn <pedjasebez3545@gmail.com>
2026-09-19 17:10:32 +0200

    Update cars-ui.js

--- cars-ui.js
+++ cars-ui.js
@@ -37,7 +37,7 @@ const VEHICLE_API_VERSION = "v13";
 const VEHICLE_CATALOG_BASE_URL =
     "https://cdn.jsdelivr.net/gh/vehiclesdb/vehiclesdb@latest/catalog";
 
-const VEHICLE_DETAILS_CACHE_VERSION = "v14";
+const VEHICLE_DETAILS_CACHE_VERSION = "v15";
 
 const MAX_SEARCH_RESULTS = 300;
 
