cf74b0b4a720052ca907037ca81038911b10a687
DarthNecrohorn <pedjasebez3545@gmail.com>
2026-09-20 13:59:26 +0200

    Update markets-ui.js

--- markets-ui.js
+++ markets-ui.js
@@ -1972,7 +1972,11 @@ async function loadMarketCardImage(
 
 
     imageElement.loading =
-        "lazy";
+        "eager";
+
+
+    imageElement.style.display =
+        "block";
 
 
     imageElement.decoding =
